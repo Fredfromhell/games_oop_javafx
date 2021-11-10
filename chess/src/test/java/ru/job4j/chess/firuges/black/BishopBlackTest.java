347cb6aad57f667ff505dacdeaaf058790aa45a0
FredfromHell <bespalov@avtocenter.ru>
2021-11-10 10:26:11 +0300

    2. Каркас шахматной доски [#792 #223993]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -2,21 +2,34 @@ package ru.job4j.chess.firuges.black;
 
 import junit.framework.TestCase;
 import ru.job4j.chess.firuges.Cell;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-public class BishopBlackTest extends TestCase {
+import org.junit.Test;
 
+public class BishopBlackTest {
+
+    @Test
     public void testPosition() {
         BishopBlack testPos = new BishopBlack(Cell.F8);
         Cell expected = Cell.F8;
-        assertEquals (expected, testPos.position());
+        assertEquals(expected, testPos.position());
 
     }
 
+    @Test
     public void testCopy() {
         BishopBlack testCop = new BishopBlack(Cell.F8);
         Cell expected = Cell.F2;
         assertEquals(expected, testCop.copy(Cell.F2).position());
 
+    }
+
+    @Test
+    public void testWay() {
+        BishopBlack test = new BishopBlack(Cell.C1);
+        Cell[] expected = new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertEquals(expected, test.way(Cell.G5));
     }
 }
